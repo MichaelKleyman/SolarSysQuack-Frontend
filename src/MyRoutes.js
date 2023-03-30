7a6d6d752ae088708f4c3125d74f99ed867d0124
Michael Kleyman <michaelkleyman99@gmail.com>
2023-03-30 09:23:32 -0400

    made the homepage for all the planets

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -9,5 +9,4 @@ const MyRoutes = () => {
     </Routes>
   );
 };
-
 export default MyRoutes;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { GiRingedPlanet } from 'react-icons/gi';
+// import useMediaQuery from '@mui/material/useMediaQuery';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { useTheme } from '@mui/material/styles';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [open, setOpen] = useState(false);
+  const [selectedPlanet, setSelectedPlanet] = useState('');
+  const theme = useTheme();
+  //   const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
 
-export default Home
+  const handleClickOpen = (name) => {
+    setOpen(true);
+    setSelectedPlanet(name);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedPlanet('');
+  };
+
+  const planets = [
+    { Name: 'Mercury' },
+    { Name: 'Venus' },
+    { Name: 'Earth' },
+    { Name: 'Mars' },
+    { Name: 'Jupiter' },
+    { Name: 'Saturn' },
+    { Name: 'Uranus' },
+    { Name: 'Neptune' },
+    { Name: 'Pluto' },
+  ];
+
+  return (
+    <div className='main-page'>
+      <div className='header'>
+        <h1>Our Solar System</h1>
+      </div>
+      <div className='planets'>
+        {planets.map((planet, i) => (
+          <div
+            key={i}
+            className='planet-border'
+            onClick={() => handleClickOpen(planet.Name)}
+          >
+            <div className='planet-detail'>
+              <div></div>
+              <h4 className='planet-name'>{planet.Name}</h4>
+              <div className='planet-icon'>
+                <GiRingedPlanet
+                  size={25}
+                  style={{
+                    color: 'blue',
+                  }}
+                />
+              </div>
+            </div>
+            <div className='individual-planet'></div>
+          </div>
+        ))}
+      </div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='responsive-dialog-title'
+      >
+        <DialogTitle id='responsive-dialog-title'>
+          {selectedPlanet} Details
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Close
+          </Button>
+          <Button onClick={handleClose} autoFocus>
+            See more
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default Home;
