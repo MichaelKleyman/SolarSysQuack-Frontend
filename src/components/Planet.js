21ace1e245f84de5cf7ef9b9a5c3701d191398ad
Michael Kleyman <michaelkleyman99@gmail.com>
2023-04-01 13:36:40 -0400

    gotbackground image for planet is setup

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router';
 import { useParams } from 'react-router';
 import { BiArrowBack } from 'react-icons/bi';
 import { Client, BASE_URL } from './api';
+import starsImg from '../images/stars.jpg';
 
 const Planet = () => {
   const [planet, setPlanet] = useState(null);
@@ -10,50 +11,68 @@ const Planet = () => {
   const navigate = useNavigate();
   const { name } = useParams();
 
-  
   useEffect(() => {
     const getPlanetData = async () => {
-      let planetData = await Client.get(`${BASE_URL}/quackBack/planet/moons/${name}`)
-      console.log(planetData.data)
-      setPlanet(planetData.data)
-    }
-    getPlanetData()
-  }, [])
+      let planetData = await Client.get(
+        `${BASE_URL}/quackBack/planet/moons/${name}`
+      );
+      console.log(planetData.data);
+      setPlanet(planetData.data);
+    };
+    getPlanetData();
+  }, []);
 
   return (
     <div>
-      <section className='planet-title'>
-        <h4 className='planet-name-page'>Let's "QUACK" About {name}</h4>
-      </section>
-      <section className='planet-image'>
-        <div className='card2'>
-          <div className='space'>
-            <div className='solarSys' id={`${name}`}>
-              <div className={`${name}Line1`}></div>
-              <div className={`${name}Line2`}></div>
-              <div className={`${name}Line3`}></div>
-              <div className={`${name}Line4`}></div>
-              <div className={`${name}Line5`}></div>
-              <div className={`${name}Rings`}></div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className='planet-buttons'>
-        <button
-          style={{ border: 'none' }}
-          className='go-back'
-          onClick={() => navigate('/')}
-        >
+      <div style={{ backgroundColor: 'rgb(0 0 0 / 0.8)' }}>
+        <img className='planet-background' src={starsImg} alt='background' />
+        <button className='go-back' onClick={() => navigate('/')}>
           <BiArrowBack size={30} style={{ paddingRight: '1rem' }} />
           Go back
         </button>
-      </section>
-      <section className='planet-detail'>
-        <p className='name'>{name}</p>
-        <p>{planet?.diameter}</p>
-        <p>{planet?.mass}</p>
-      </section>
+        <section className='planet-title'>
+          <h4 className='planet-name-page'>
+            Let's "QUACK" About <span style={{ color: 'blue' }}>{name}</span>
+          </h4>
+        </section>
+        <section className='planet-image'>
+          <div className='card2'>
+            <div className='space'>
+              <div className='solarSys' id={`${name}`}>
+                <div className={`${name}Line1`}></div>
+                <div className={`${name}Line2`}></div>
+                <div className={`${name}Line3`}></div>
+                <div className={`${name}Line4`}></div>
+                <div className={`${name}Line5`}></div>
+                <div className={`${name}Rings`}></div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className='planet-buttons'></section>
+        <section className='planet-detail-container'>
+          <div>
+            <p className='description-label'>Planet Mass</p>
+            <p className='planet-detail'>{planet?.mass}</p>
+          </div>
+          <div>
+            <p className='description-label'>Equatorial Diameter</p>
+            <p className='planet-detail'>{planet?.diameter}</p>
+          </div>
+          <div>
+            <p className='description-label'>Distance From Sun</p>
+            <p className='planet-detail'>{planet?.distance}</p>
+          </div>
+          <div>
+            <p className='description-label'>Planet Type</p>
+            <p className='planet-detail'>{planet?.category}</p>
+          </div>
+        </section>
+      </div>
+      <div className='mini-navbar'>
+        <p>Facts</p>
+        <p>Moons</p>
+      </div>
       <section className='planet-moons'>
         {planet?.moons.map((moon, index) => (
           <div key={index}>
