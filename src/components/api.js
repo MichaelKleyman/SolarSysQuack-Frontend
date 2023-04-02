feb2ad1324c0c62bfed5369b9032ca808e5575c6
Patrick Knight <pfknight2@gmail.com>
2023-04-02 18:03:06 -0400

    added backend route to BASE_URL

--- src/components/api.js
+++ src/components/api.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
 
-export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL = 'https://solarsysquack-i5fu.onrender.com'; // 'http://localhost:3001';
 
 export const Client = Axios.create({ baseURL: BASE_URL });
